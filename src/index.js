585748fe08c8c5f00b639130f6dbfbe502e4f590
AnthonyUPTN <tagirovantn@gmail.com>
2022-04-16 12:22:09 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import './css/styles.css';
+'use strict';
 
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountry } from './fetchCountries';
+import makeCard from './handlebars/card.hbs';
+import makeList from './handlebars/list.hbs';
+
+const inputEl = document.querySelector('#search-box');
+const listSimilarResults = document.querySelector('.country-list');
 const DEBOUNCE_DELAY = 300;
+
+const searchingFunction = event => {
+  let searchWord = event.target.value;
+
+  fetchCountry(searchWord)
+    .then(data => {
+      if (data.length > 8) {
+        console.log('too much countries found');
+        return;
+      }
+      listSimilarResults.insertAdjacentHTML('beforeend', makeCard(data));
+    })
+    .catch(error => console.log(error));
+};
+
+inputEl.addEventListener('input', debounce(searchingFunction, DEBOUNCE_DELAY));
